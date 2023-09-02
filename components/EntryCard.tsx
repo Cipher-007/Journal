9497493c94230f880b5854e65ca686b975ca40ae
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-02 13:28:10 +0530

    entry component

---
+++ components/EntryCard.tsx
@@ -0,0 +1,21 @@
+export default function EntryCard({
+  entry,
+}: {
+  entry: {
+    id: string;
+    createdAt: Date;
+    analysis: {
+      summary: string;
+      mood: string;
+    };
+  };
+}) {
+  const date = new Date(entry.createdAt).toDateString();
+  return (
+    <div className='divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow'>
+      <div className='px-4 py-5 sm:px-6'>{date}</div>
+      <div className='px-4 py-5 sm:p-6'>{entry.analysis.summary}</div>
+      <div className='px-4 py-4 sm:px-6'>{entry.analysis.mood}</div>
+    </div>
+  );
+}
