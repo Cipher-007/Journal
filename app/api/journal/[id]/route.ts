e7cef74843c28ef6be5ad750574090bd8ff60c65
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-02 13:25:26 +0530

    update journal entry route

---
+++ app/api/journal/[id]/route.ts
@@ -0,0 +1,48 @@
+import { analyze } from "@/utils/ai";
+import { getUserByClerkId } from "@/utils/auth";
+import { prisma } from "@/utils/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { content } = await req.json();
+  const user = await getUserByClerkId();
+  const updatedEntry = await prisma.journalEntry.update({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id: params.id,
+      },
+    },
+    data: {
+      content: content,
+    },
+  });
+
+  const analysis = await analyze(updatedEntry.content);
+  let updatedAnalysis;
+  if (analysis) {
+    updatedAnalysis = await prisma.analysis.upsert({
+      where: {
+        entryId: updatedEntry.id,
+      },
+      create: {
+        userId: user.id,
+        entryId: updatedEntry.id,
+        color: analysis.color,
+        mood: analysis.mood,
+        negative: analysis.negative,
+        subject: analysis.subject,
+        summary: analysis.summary,
+        sentimentScore: analysis.sentimentScore,
+      },
+      update: analysis,
+    });
+  }
+
+  return NextResponse.json({
+    data: { ...updatedEntry, analysis: updatedAnalysis },
+  });
+}
