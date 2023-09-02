f3cec25c78aa38f81a71610ee9c6da5c48b05094
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-02 13:23:01 +0530

    journal id page

---
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -0,0 +1,43 @@
+import Editor from "@/components/Editor";
+import { getUserByClerkId } from "@/utils/auth";
+import { prisma } from "@/utils/db";
+
+async function getEntry(id: string) {
+  const user = await getUserByClerkId();
+
+  const entry = await prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id: id,
+      },
+    },
+    select: {
+      id: true,
+      content: true,
+      analysis: true,
+    },
+  });
+  return entry;
+}
+
+export default async function EntryPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const entry = await getEntry(params.id);
+  if (entry !== null) {
+    return (
+      <div className='h-full w-full'>
+        <Editor
+          entry={{
+            id: entry.id,
+            content: entry.content,
+            analysis: entry.analysis!,
+          }}
+        />
+      </div>
+    );
+  }
+}
