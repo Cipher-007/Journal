4b94986a6804f05bae85b14ea4a148e748facf06
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-02 13:26:34 +0530

    OpenAI question route

---
+++ app/api/question/route.ts
@@ -0,0 +1,24 @@
+import { qa } from "@/utils/ai";
+import { getUserByClerkId } from "@/utils/auth";
+import { prisma } from "@/utils/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { question } = await req.json();
+  const user = await getUserByClerkId();
+
+  const entires = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    select: {
+      id: true,
+      content: true,
+      createdAt: true,
+    },
+  });
+
+  const answer = await qa(question, entires);
+
+  return NextResponse.json({ data: answer });
+}
