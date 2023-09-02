8b9cea800c7d14925404786a4e8dd1a68179e66e
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-02 13:24:26 +0530

    create journal route

---
+++ app/api/journal/route.ts
@@ -0,0 +1,36 @@
+import { analyze } from "@/utils/ai";
+import { getUserByClerkId } from "@/utils/auth";
+import { prisma } from "@/utils/db";
+import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const data = await req.json();
+  const user = await getUserByClerkId();
+  const entry = await prisma.journalEntry.create({
+    data: {
+      userId: user.id,
+      content: data.content,
+    },
+  });
+
+  const analysis = await analyze(entry.content);
+
+  if (analysis) {
+    await prisma.analysis.create({
+      data: {
+        userId: user.id,
+        entryId: entry.id,
+        color: analysis.color,
+        mood: analysis.mood,
+        negative: analysis.negative,
+        subject: analysis.subject,
+        summary: analysis.summary,
+        sentimentScore: analysis.sentimentScore,
+      },
+    });
+  }
+
+  revalidatePath("/journal");
+  return NextResponse.json({ data: entry });
+}
