7b261770ec68a832ba6042d787046e26e2ad1530
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-25 15:34:56 +0530

    removed useAutosave, new design and added error handling

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,64 +1,70 @@
 "use client";
 
-import { Analysis } from "@/types";
 import { updateEntry } from "@/utils/api";
 import { useState } from "react";
-// @ts-ignore
-import { useAutosave } from "react-autosave";
+import { Button } from "./ui/button";
+import type { Analysis } from "@prisma/client";
+import AnalysisCard from "./AnalysisCard";
+import { Textarea } from "./ui/textarea";
 
 export default function Editor({
   entry,
 }: {
-  entry: { id: string; content: string; analysis: Analysis };
+  entry: { id: string; content: string; analysis: Partial<Analysis> };
 }) {
   const [value, setValue] = useState(entry.content);
   const [isLoading, setIsLoading] = useState(false);
   const [analysis, setAnalysis] = useState(entry.analysis);
 
-  const { mood, summary, color, subject, negative } = analysis;
-  const analysisData = [
-    { name: "Summary", value: summary },
-    { name: "Subject", value: subject },
-    { name: "Mood", value: mood },
-    { name: "Negative", value: negative ? "True" : "False" },
-  ];
+  async function handleSave() {
+    setIsLoading(true);
+    console.log(value);
+    const { updatedEntry, error } = await updateEntry(entry.id, value);
+    if (updatedEntry) {
+      setAnalysis(updatedEntry.analysis);
+    }
 
-  useAutosave({
-    data: value,
-    onSave: async (_value: string) => {
-      setIsLoading(true);
-      const data = await updateEntry(entry.id as string, _value);
-      setAnalysis(data.analysis);
-      setIsLoading(false);
-    },
-  });
+    if (error) {
+      console.error(error);
+    }
+    setIsLoading(false);
+  }
+
   return (
-    <div className='w-full h-full grid grid-cols-3'>
-      <div className='col-span-2'>
-        {isLoading && <div>...loading</div>}
-        <textarea
-          className='w-full h-full p-8 text-xl outline-none'
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </div>
-      <div className='border-l border-black/10'>
-        <div className='px-6 py-10' style={{ backgroundColor: color }}>
-          <h2 className='text-2xl'>Analysis</h2>
-        </div>
-        <div>
-          <ul>
-            {analysisData.map((item) => (
-              <li
-                key={item.name}
-                className='flex items-center justify-between px-2 py-4 border-b border-t border-black/10'
-              >
-                <span className='text-lg font-semibold'>{item.name}</span>
-                <span>{item.value}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div className='h-[95%] flex flex-row pt-4 pl-2'>
+      <AnalysisCard analysis={analysis} className='basis-1/5' />
+      <div className='basis-4/5 px-4'>
+        {isLoading ? (
+          <div className='flex items-center justify-center h-full w-full'>
+            <svg
+              aria-hidden='true'
+              className='w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
+              viewBox='0 0 100 101'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+                fill='currentColor'
+              />
+              <path
+                d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+                fill='currentFill'
+              />
+            </svg>
+          </div>
+        ) : (
+          <>
+            <Textarea
+              className='w-full h-full p-8 text-xl outline-none dark:text-black border-black border-2 rounded'
+              value={value}
+              onChange={(event) => setValue(event.target.value)}
+            />
+            <div className='text-right pr-4 pt-4'>
+              <Button onClick={handleSave}>Save</Button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
