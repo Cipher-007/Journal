ee744d9f3ae5df268d9d531bc8cb2e5b567aa3c8
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-25 15:38:29 +0530

    added loading state and error handling

--- components/NewEntry.tsx
+++ components/NewEntry.tsx
@@ -1,23 +1,37 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { createNewEntry } from "@/utils/api";
+import { Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
-export default function NewEntryCard() {
+export default function NewEntry({ className }: { className?: string }) {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   async function clickHandler() {
+    setLoading(true);
     const data = await createNewEntry();
-    router.push(`/journal/${data.id}`);
+    if (data.entry) {
+      router.push(`/journal/${data.entry.id}`);
+    } else if (data.error) {
+      console.error(data.error);
+    }
+    setLoading(false);
   }
   return (
     <div
-      className='cursor-pointer overflow-hidden rounded-lg bg-white shadow'
-      onClick={clickHandler}
+      className={cn(
+        "flex items-center justify-center transition-all duration-200 ease-in-out hover:scale-105",
+        className
+      )}
     >
-      <div className='px-4 py-5 sm:p-6'>
-        <span className='text-3xl'>New Entry</span>
-      </div>
+      <Button onClick={clickHandler} className='bg-blue-600' disabled={loading}>
+        <Plus className='pl-1 h-5 w-5' />
+        New Entry
+      </Button>
     </div>
   );
 }
