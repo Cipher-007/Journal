9a44d891f924ab58b7d0b004e2eb8e28d7c3c7f8
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-26 15:03:13 +0530

    added types

--- types.ts
+++ types.ts
@@ -1,1 +1,21 @@
+import type { Analysis } from "@prisma/client";
 
+export type EntryCardProps = {
+  id: string;
+  createdAt: Date;
+  analysis: Partial<Analysis> | null;
+};
+
+export type ChatProps = {
+  id: string;
+  user: string;
+  ai: string;
+}[];
+
+export type JournalEntryWithAnalysis = {
+  id: string;
+  content: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  analysis: Partial<Analysis> | null;
+};
