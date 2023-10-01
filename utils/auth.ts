cb80ebf9cfc9a8f3839e8cdd4197e27b03adf1f5
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-10-01 13:38:11 +0530

    removed keyfetch function

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,7 +1,5 @@
 import { auth } from "@clerk/nextjs";
 import { prisma } from "./db";
-import { currentUser } from "@clerk/nextjs/server";
-import { decrypt } from "./aes";
 
 export async function getUserByClerkId() {
   const { userId } = auth();
@@ -12,19 +10,4 @@ export async function getUserByClerkId() {
     },
   });
   return user;
-}
-
-export async function keyfetch() {
-  const user = await currentUser();
-
-  const openAIKey = await prisma.user.findUnique({
-    where: {
-      clerkId: user?.id as string,
-    },
-    select: {
-      openAI: true,
-    },
-  });
-  const key = decrypt(openAIKey?.openAI as string);
-  return key;
 }
